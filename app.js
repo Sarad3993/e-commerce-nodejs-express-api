4967558aeccfde38dfa61cecf04f18b9ae3520df
Sarad3993 <risingstar3993@gmail.com>
2023-03-04 15:35:20 +0545

    connect to database

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require ("express");
 const app = express();
 
-
+// database 
+const connectDB = require("./db/connect");
 
 
 
@@ -14,6 +15,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () =>{
     try{
+        await connectDB(process.env.MONGO_URI); // since it is returning a promise we need to await and pass the url
         app.listen(port, console.log(`Server is running on port ${port}...`));
     }catch(err){
         console.log(err);
---
+++ db/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery",false);
+
+
+// here we are connecting to the database using the url provided in the .env file and exporting the connection to the caller in the app.js file
+const connectDB = (url) => {
+    return mongoose.connect(url);
+}
+
+module.exports = connectDB;
