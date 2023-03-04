95d0d455c1d4937e4f29ea2ba662be65b68cd1b2
Sarad3993 <risingstar3993@gmail.com>
2023-03-04 16:26:24 +0545

    set up basic route

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
+require("express-async-errors"); // express async errors is a middleware that handles all the errors thrown by the async functions instead of adding try catch blocks in every async function
+
 const express = require ("express");
 const app = express();
 
 // database 
 const connectDB = require("./db/connect");
 
+// middlewares 
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 
+app.use(express.json()) // this is a middleware that parses the incoming request with JSON payloads and is based on body-parser so we don't need to install body-parser separately
+
+app.get('/',(req,res)=>{
+    res.send('e-commerce-api');
+})
 
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 const port = process.env.PORT || 3000;
