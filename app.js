f7f3948c846f58d6aa4f055b29b411bb266f94ad
Sarad3993 <risingstar3993@gmail.com>
2023-03-04 15:24:35 +0545

    basic express server setup

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("E-commerce API");
+require("dotenv").config();
+const express = require ("express");
+const app = express();
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () =>{
+    try{
+        app.listen(port, console.log(`Server is running on port ${port}...`));
+    }catch(err){
+        console.log(err);
+    }
+}
+
+start();
