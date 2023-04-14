4d4c15e76a01457296ba178ad9f025a1154f4108
Sarad3993 <risingstar3993@gmail.com>
2023-04-14 17:00:33 +0545

    cookies setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,8 +17,15 @@ const register = async (req,res) => {
     const tokenUser = {name:user.name,userId:user._id,role:user.role};
 
     const token = createJWT({payload:tokenUser});
+
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    res.cookie('token',token,{
+      httpOnly: true,
+      expires: new Date(Date.now() + oneDay),
+    })
     
-    res.status(StatusCodes.CREATED).json({user:tokenUser,token});
+    res.status(StatusCodes.CREATED).json({user:tokenUser});
 }
 
 const login = async (req, res) => {
