e0c7f60b81cb528ba39902010f13bbf81c745626
Sarad3993 <risingstar3993@gmail.com>
2023-06-02 13:12:31 +0545

    createTokenUser function

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,7 @@
 const {createJWT,isTokenValid , attachCookiesToResponse} = require('./jwt');
 
+const createTokenUser = require('./createTokenUser')
+
 module.exports = {
-    createJWT, isTokenValid , attachCookiesToResponse
+    createJWT, isTokenValid , attachCookiesToResponse , createTokenUser
 };
