68537c141c4661f97b3031a4b13b1bebd215989c
Sarad3993 <risingstar3993@gmail.com>
2023-06-07 14:51:47 +0545

    get all reviews and get single review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -25,14 +25,25 @@ const createReview = async (req,res)=>{
     res.status(StatusCodes.CREATED).json({review});
 }
 const getAllReviews = async (req,res)=>{
-    res.send('get all reviews');
+    const reviews = await Review.find({});
+    res.status(StatusCodes.OK).json({reviews, count:reviews.length});
 }
 const getSingleReview = async (req,res)=>{
-    res.send('get single review');
+    const {id: reviewId} = req.params;
+
+    const review = await Review.findOne({_id:reviewId});
+
+    if(!review){
+        throw new CustomError.NotFoundError(`No review with id: ${reviewId}`)
+    }
+    res.status(StatusCodes.OK).json({review});
+    
 }
+
 const updateReview = async (req,res)=>{
     res.send('update review');
 }
+
 const deleteReview = async (req,res)=>{
     res.send('delete review');
 }
