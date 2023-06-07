f1f6bcd728a21b97417cd858b09d0a5cc8e772a0
Sarad3993 <risingstar3993@gmail.com>
2023-06-07 14:44:34 +0545

    create review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,28 @@
+const Review = require("../models/Review");
+const Product = require("../models/Product");
+const {StatusCodes} = require("http-status-codes");
+const CustomError = require("../errors");
+const {checkPermissions} = require("../utils");
 
 
 const createReview = async (req,res)=>{
-    res.send('Create review');
+    const { product: productId } = req.body 
+
+    const isValidProduct = await Product.findOne({_id:productId})
+
+    if(!isValidProduct){
+        throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+    }
+
+    const alreadySubmittedReview = await Review.findOne({product: productId,
+    user: req.user.userId});
+    if(alreadySubmittedReview){
+        throw new CustomError.BadRequestError(`You have already submitted a review for this product`);
+    }
+
+    req.body.user = req.user.userId;
+    const review = await Review.create(req.body);
+    res.status(StatusCodes.CREATED).json({review});
 }
 const getAllReviews = async (req,res)=>{
     res.send('get all reviews');
--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,5 +1,4 @@
 
-
 const createTokenUser = (user) => {
     return {name:user.name, userId: user._id, role:user.role }
 }
